be7fb6b8fe43446ae0900797e8739f80beb2f771
ogura018 <72480089+ogura018@users.noreply.github.com>
2021-06-11 11:52:20 +0900

    master

--- js/slide.js
+++
@@ -1,8 +0,0 @@
-
-jQuery(function ($) {
-	$('.bgimg-slider').bgSwitcher({
-		images: ['./images/pic_bg.jpg', '../images/pic_bg1.jpg', '../images/pic_bg2/.jpg'],
-		effect: "drop",
-		easing: "linear"
-	});
-});
